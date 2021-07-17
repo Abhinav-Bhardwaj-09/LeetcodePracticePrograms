13c970164a579a7aace20daed51e5b9901f273a4
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-07-17 22:33:23 +0530

    After adding another solution of Problem 35. Search Insert Position (Optimised Binary Search)

--- src/SearchInsertPosition_35.java
+++ src/SearchInsertPosition_35.java
@@ -53,6 +53,37 @@ public class SearchInsertPosition_35 {
     }
     /**/
 
+   /* Optimised Binary Search */
+   public int searchInsert(int[] nums, int target) {
+       int start = 0;
+       int end = nums.length-1;
+       int mid;
+
+       if(nums[0] >= target) {
+           return 0;
+       }
+
+       while(start <= end) {
+           mid = start + (end-start) / 2;
+
+           if(nums[mid] > target) {
+               end = mid - 1;
+           }
+           else if(nums[mid] < target) {
+               start = mid + 1;
+           }
+           else {
+
+               return mid;
+           }
+       }
+
+       if(start > end) {
+           return start;
+       }
+
+       return nums.length;
+   }
+    /**/
 
 }
-
